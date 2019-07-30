6c344ff03312311341bd625d590aded6e14d8e5b
LucyMHall <lucymhall1995@gmail.com>
2019-07-30 09:41:31 +0100

    TASK: add #format to BankStatementCreator to format a withdrawal transaction

--- lib/bank_statement_creator.rb
+++ lib/bank_statement_creator.rb
@@ -2,12 +2,21 @@ class BankStatementCreator
 
   def print_bank_statement(record)
     print_header
+    print record.map{|transaction| format(transaction)}.join
   end
 
   private
 
   def print_header
-      print "date || credit || debit || balance"
+      print "date || credit || debit || balance \n "
+  end
+
+  def format(transaction)
+    if transaction[:type] == :withdraw
+      "#{transaction[:date]} || || #{transaction[:amount]}0 || #{transaction[:new_balance]}0 \n "
+    # else
+    #   "#{transaction[:date]} || #{transaction[:amount]}0 || || #{transaction[:new_balance]}0 \n "
+    end
   end
 
 end
--- spec/bank_statement_creator_spec.rb
+++ spec/bank_statement_creator_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe BankStatementCreator do
     it "displays an empty table if it is passed an empty record" do
       @bank_statement_creator = BankStatementCreator.new
       record = []
-      expect{@bank_statement_creator.print_bank_statement(record)}.to output("date || credit || debit || balance").to_stdout
+      expect{@bank_statement_creator.print_bank_statement(record)}.to output("date || credit || debit || balance \n ").to_stdout
+    end
+
+    it "displays headers and a formatted record with withdrawal amount in debit column" do
+      @bank_statement_creator = BankStatementCreator.new
+      record = [{:type => :withdraw, :amount => 10.00, :date => "10-01-2012", :new_balance => -10.00}]
+      expected_display = "date || credit || debit || balance \n 10-01-2012 || || 10.00 || -10.00 \n "
+      expect{@bank_statement_creator.print_bank_statement(record)}.to output(expected_display).to_stdout
     end
 
   end
